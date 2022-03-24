9507c2d9ee3c8151e536a4f58b8068eaa5801e2b
LZL <1501683027@qq.com>
2022-03-24 09:25:54 +0800

    second commit

--- src/main/java/com/lzl/git/gitTest.java
+++ src/main/java/com/lzl/git/gitTest.java
@@ -3,5 +3,6 @@ package com.lzl.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello lzl git !!!!!!!!!!!");
+        System.out.println("这是版本 222222号");
     }
 }
