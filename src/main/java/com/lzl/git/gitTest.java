70ad08e8024c415d1cb4cabbd1a57b9f7ef49b32
LZL <1501683027@qq.com>
2022-03-24 11:00:24 +0800

    hot-fix-commit-3

--- src/main/java/com/lzl/git/gitTest.java
+++ src/main/java/com/lzl/git/gitTest.java
@@ -5,7 +5,7 @@ public class gitTest {
         System.out.println("hello lzl git !!!!!!!!!!!");
         System.out.println("这是版本 222222号");
         System.out.println("我是hot-fix，现在测试合并分支");
-        System.out.println("w我是master，现在同一行 测试合并冲突");
+        System.out.println("w我是master，现在不不不不不不同一行 测试合并冲突");
         System.out.println("我是hot-fix，现在测试合并分支-----中的冲突合并");
     }
 }
